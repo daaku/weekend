54b8ed1125c543cfc5c54acdace32feaf38c8ae5
Naitik Shah <naitik.shah@yahoo.com>
2009-02-21 05:30:13 +0000

    added static file support

--- weekend/urls.py
+++ weekend/urls.py
@@ -6,9 +6,13 @@ from weekend.fireeagle_oauth import app as fireeagle_oauth
 
 admin.autodiscover()
 
+import os.path as path
+MEDIA_DIR = path.realpath(path.join(path.dirname(__file__), '..', 'media'))
+
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^oauth/yahoo/', include(yahoo_oauth.urls)),
     (r'^oauth/fireeagle/', include(fireeagle_oauth.urls)),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_DIR}),
     (r'', include('weekend.common.urls')),
 )
