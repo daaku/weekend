4aadfb4ce0ca87fe6486227ab9f62a8058bdb7e7
Dustin Whittle <dustin.whittle@gmail.com>
2009-02-22 05:47:20 +0000

    updated models to make reviews based on items

--- weekend/common/models.py
+++ weekend/common/models.py
@@ -5,9 +5,15 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     guid = models.CharField(max_length=64)
     access_token = models.CharField(max_length=128)
-    
+    def __unicode__(self):
+        return self.user.first_name
+
+
 class Review(models.Model):
     user = models.ForeignKey(User)
     restaurant = models.CharField(max_length=254)
-    stars = models.IntegerField()
-    content = models.CharField(max_length=1000)
+    item = models.CharField(max_length=254)
+    votes = models.IntegerField()
+    def __unicode__(self):
+        return self.item
+
--- weekend/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from weekend.models import Review
-
-class ReviewForm(ModelForm):
-    class Meta:
-        model = Review
