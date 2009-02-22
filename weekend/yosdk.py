73a95318bcce81f3a8b0ca709fa124b5fb9e7f61
Chirag Shah <chiragshah1@gmail.com>
2009-02-22 21:44:47 +0000

    Updated the yosdk

--- weekend/yosdk.py
+++ weekend/yosdk.py
@@ -3,7 +3,10 @@ from weekend.yahoo_oauth import app as yahoo_oauth
 import time
 import logging
 import json
+import random
 
+YOS_URL='http://social.yahooapis.com/v1'
+YQL_URL='http://query.yahooapis.com/v1/yql'
 
 def social_graph(request):
     access_token = request.session['yahoo_access_token']
@@ -13,34 +16,29 @@ def social_graph(request):
     }
     response = yahoo_oauth.make_signed_req(YQL_URL, content=params, token=access_token, request=request)
     body = json.loads(unicode(response.read(), 'utf-8'))['query']['results']['profile']
-    logging.debug(body[0])
     return body
 
-
-def updates(request):
+def updates(request, descr, title, link):
     access_token = request.session['yahoo_access_token']
     guid = access_token['xoauth_yahoo_guid']
-    descr = "insert description" #FIXME
-    title = "insert title" #FIXME
-    link = "http://daaku.org" #FIXME
     source = "APP.JUqAuh5g"
-    suid = "installed3"
+    suid = random.randrange(0, 101)
     body = '''
-{ "updates":
-  [
-    {
-      "class": "app",
-      "collectionType": "guid",
-      "description": "%s",
-      "suid": "%s",
-      "link": "%s",
-      "source": "%s",
-      "pubDate": "%s",
-      "title": "%s",
-      "type": "appActivity",
-      "collectionID": "%s"
-    }
-  ]
-}''' % (descr, suid, link, source, int(time.time()), title, guid)
+    { "updates":
+        [
+            {
+                "class": "app",
+                "collectionType": "guid",
+                "description": "%s",
+                "suid": "%s",
+                "link": "%s",
+                "source": "%s",
+                "pubDate": "%s",
+                "title": "%s",
+                "type": "appActivity",
+                "collectionID": "%s"
+            }
+        ]
+    }''' % (descr, suid, link, source, int(time.time()), title, guid)
     response = yahoo_oauth.make_signed_req("%s/user/%s/updates/%s/%s" % (YOS_URL, guid, source, suid), method='PUT', content=body, token=access_token, request=request)
     return response.status
