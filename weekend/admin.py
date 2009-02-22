f14f45deb1e1b03319479f9a75fab7e628f8916a
Dustin Whittle <dustin.whittle@gmail.com>
2009-02-22 03:44:19 +0000

    fixed fireagle auth key is required + added admin + models

---
+++ weekend/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from weekend.models import UserProfile
+
+admin.site.register(UserProfile)
---
+++ weekend/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    guid = models.CharField(max_length=64)
+    access_token = models.CharField(max_length=128)
+    
+class Review(models.Model):
+    user = models.ForeignKey(User)
+    restaurant_id = models.CharField(max_length=254)
+    stars = models.IntegerField()
+    content = models.CharField(max_length=1000)
+
