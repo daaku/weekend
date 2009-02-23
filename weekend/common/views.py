1accc5356684a4fcb5906675c526dae4327758a8
Dustin Whittle <dustin.whittle@gmail.com>
2009-02-23 00:09:13 +0000

    updated yql public api + update view for common/places

--- weekend/common/views.py
+++ weekend/common/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response as render
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.html import escape
@@ -16,7 +17,7 @@ import collections
 import json
 
 YQL_URL='http://query.yahooapis.com/v1/yql'
-YQL_PUBLIC_URL='http://query.yahooapis.com/v1/yql'
+YQL_PUBLIC_URL='http://query.yahooapis.com/v1/public/yql'
 FIREEAGLE_USER_URL='https://fireeagle.yahooapis.com/api/0.1/user.json'
 YELP_REVIEW_URL='http://api.yelp.com/business_review_search'
 
@@ -97,18 +98,30 @@ def all_menus_yql(request):
     return HttpResponse(unicode(response.read(), 'utf-8'))
 
 def places(request):
+  
+    if 'lat' in request.GET and 'lon' in request.GET:
 
-    lat = request.GET['lat']     # 37.4248085022
-    lon = request.GET['lon']     # -122.074012756
+      lat = request.GET['lat']     # 37.4248085022
+      lon = request.GET['lon']     # -122.074012756
     
-    params = {
-        'q': "select * from xml where url='http://api.boorah.com/restaurants/WebServices/RestaurantSearch?radius=5&sort=distance&start=0&lat=%s&long=%s&auth=%s' and itemPath = 'Response.ResultSet.Result'" % (lat, lon, settings.BOORAH_API_KEY),
-        'format': 'json',
-    }
+      params = {
+          'q': "select * from xml where url='http://api.boorah.com/restaurants/WebServices/RestaurantSearch?radius=5&sort=distance&start=0&lat=%s&long=%s&auth=%s' and itemPath = 'Response.ResultSet.Result'" % (lat, lon, settings.BOORAH_API_KEY),
+          'format': 'json',
+      }
 
-    restaurants = json.loads(unicode(yahoo_oauth.make_signed_req(YQL_PUBLIC_URL, content=params).read(), 'utf-8'))['query']['results']['Result']
+      response = json.loads(unicode(yahoo_oauth.make_signed_req(YQL_PUBLIC_URL, content=params).read(), 'utf-8'))
 
-    return HttpResponse(render('common/places.html', { 'places': restaurants }))
+      if 'query' in response:
+        restaurants = response['query']['results']['Result']
+      else:
+        restaurants = []
+        
+      return HttpResponse(render('common/places.html', { 'places': restaurants }))
+
+    else:
+      
+      return HttpResponseRedirect('/')
+
 
 def items(request):
   
@@ -118,8 +131,7 @@ def items(request):
 
     # allmenus_yql = select * from html where url='http://www.allmenus.com/ca/mountain-view/123349-quiznos-sub/menu/' and xpath='//div[@class="menu_item"]'
     # menupages_yql = select * from html where url='http://www.menupages.com/Partnermenu.asp?partner=7&restaurantId=10522&t=1235342717&auth=4b479e7b075fef07b533cd1acee30369' and xpath='//div[@id="restaurant-menu"]/table/tbody/tr/th'
-    
-    access_token = request.session['yahoo_access_token']
+
     params = {
         'q': "select * from xml where url='http://api.boorah.com/restaurants/WebServices/RestaurantSearch?radius=5&sort=distance&start=0&lat=%s&long=%s&auth=%s' and itemPath = 'Response.ResultSet.Result'" % (lat, lon, settings.BOORAH_API_KEY),
         'format': 'json',
