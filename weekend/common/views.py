33726455aaf16b893723723d9139d7de7e1e89bb
Dustin Whittle <dustin.whittle@gmail.com>
2009-02-23 03:32:22 +0000

    fixed error in views

--- weekend/common/views.py
+++ weekend/common/views.py
@@ -149,7 +149,7 @@ def menu(request):
 
             pass
 
-    return HttpResponse(render('common/menu.html', { 'menu': menu, 'restaurant': quote(restaurant) }))
+        return HttpResponse(render('common/menu.html', { 'menu': menu, 'restaurant': quote(restaurant) }))
 
     else:
       
